d34923680fb371b7272e801f0cdb6163882237a4
jdiveley@diveley.net <jdiveley@diveley.net>
2025-07-01 22:07:03 +0000

    taiwind

---
+++ src/Pages/About.jsx
@@ -0,0 +1,10 @@
+
+
+export const About=() => {
+
+    return(
+    	<div>
+	  <h1 className="text-green-500">About</h1>
+	</div>
+    )	
+}
---
+++ src/Pages/Terms.jsx
@@ -0,0 +1,10 @@
+
+
+export const Terms=() => {
+
+    return(
+    	<div>
+	  <h1>Terms</h1>
+	</div>
+    )	
+}
